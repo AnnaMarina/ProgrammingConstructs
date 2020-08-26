7e8933cea8fc6ffcc5cb714ff86ed0c3026dc43e
AnnaMarina <anna.marina996@gmail.com>
2020-08-26 20:02:39 +0530

    Doublet Percentage stored

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,26 +1,79 @@
 #!/bin/bash
 declare -A combination
+declare -A percent
 single() {
-	tcount=0; hcount=0;
-	read -p "Enter the limit : " limit
-	for ((i=0; i<$limit; i++))
+	countT=0; countH=0;
+
+	for ((i=0; i<$1; i++))
 	do
 		choice=$((RANDOM%2))
 		if [ $choice -eq 0 ]; then
-			hcount=$(($hcount+1));
-			combination[H]=$hcount;
+			countH=$(($countH+1));
 		else
-			tcount=$(($tcount+1));
-			combination[T]=$tcount;
+			countT=$(($countT+1));
 		fi
 	done
 
-	headPercent=`printf %.3f "$(($hcount*100*100/$limit))e-2"`;
-	tailPercent=`printf %.3f "$(($tcount*100*100/$limit))e-2"`;
-	echo "${!combination[@]}";
-	echo "${combination[@]}";
-	echo "Percentage of head : "$headPercent;
-	echo "Percentage of tail : "$tailPercent;
+	combination[H]=$countH;
+	combination[T]=$countT;
 }
 
-single
+doublet() {
+	countHH=0; countHT=0; countTH=0; countTT=0;
+
+	for ((i=0; i<$1; i++))
+	do
+		choice=$((RANDOM%4));
+		case $choice in
+			0)
+				countHH=$(($countHH+1))
+			;;
+
+			1)
+				countHT=$(($countHT+1))
+			;;
+
+			2)
+				countTH=$(($countTH+1))
+			;;
+
+			3)
+				countTT=$(($countTT+1))
+			;;
+
+		esac
+	done
+	combination[HH]=$countHH;
+	combination[HT]=$countHT;
+	combination[TH]=$countTH;
+	combination[TT]=$countTT;
+
+	i=0;
+
+	#For calculating percentage of singlet and doublet.
+	for n in ${!combination[@]}
+	do
+		temp=`printf "${combination[$n]}"`
+		percent[$(($i))]=`printf %.3f "$(($temp*100*100/$1))e-2"`;
+		key[$(($i))]=`printf "$n"`;
+		i=$(($i+1));
+	done
+
+	#To print the single and doublet
+	echo "The single and doublet are";
+	echo "${!combination[@]}";
+        echo "${combination[@]}";
+
+	echo "The percentage are as follows";
+	for ((j=o; j<i; j++ ))
+	do
+	echo "${key[$j]}  :  ${percent[$j]}"
+
+	done
+	#echo "${key[@]}";
+	#echo "${percent[@]}";
+}
+	read -p "Enter the limit : " limit
+
+	single $limit
+	doublet $limit
