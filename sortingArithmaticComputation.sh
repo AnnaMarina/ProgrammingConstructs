549e209c40a2da1950f2cc90770caf9b08096a21
AnnaMarina <anna.marina996@gmail.com>
2020-08-26 12:44:32 +0530

    Computation

--- sortingArithmaticComputation.sh
+++ sortingArithmaticComputation.sh
@@ -6,3 +6,8 @@ declare -A arithmatic
         read num1;
         read num2;
         read num3;
+
+ arithmatic[a*b+c]=$(($num1*$num2+$num3));
+        arithmatic[a+b*c]=$(($num1+$num2*$num3));
+        arithmatic[c+a/b]=$(($num3+$num1/$num2));
+        arithmatic[a%b+c]=$(($num1%$num2+$num3));
