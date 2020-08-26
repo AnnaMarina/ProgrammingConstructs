fd6969ee97626915d4b06f02f1726dc7b712e1c0
AnnaMarina <anna.marina996@gmail.com>
2020-08-26 17:19:17 +0530

    Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+declare -A combination
+single() {
+	tcount=0; hcount=0;
+	read -p "Enter the limit : " limit
+	for ((i=0; i<$limit; i++))
+	do
+		choice=$((RANDOM%2))
+		if [ $choice -eq 0 ]; then
+			hcount=$(($hcount+1));
+			combination[H]=$hcount;
+		else
+			tcount=$(($tcount+1));
+			combination[T]=$tcount;
+		fi
+	done
 
-	choice=$((RANDOM%2))
-	if [ $choice -eq 0 ]; then
-		echo "Heads.";
-	else
-		echo "Tails.";
-	fi
+	headPercent=`printf %.3f "$(($hcount*100*100/$limit))e-2"`;
+	tailPercent=`printf %.3f "$(($tcount*100*100/$limit))e-2"`;
+	echo "${!combination[@]}";
+	echo "${combination[@]}";
+	echo "Percentage of head : "$headPercent;
+	echo "Percentage of tail : "$tailPercent;
+}
+
+single
