8de0b65d1824d9bcc29bba080092e50f2e1bb06d
AnnaMarina <anna.marina996@gmail.com>
2020-08-26 15:01:21 +0530

    Tie condition

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+
+	tie () {
+	temp=0;
+	while [ $temp -lt 2 ]
+	do
+		choice=$((RANDOM%2))
+                if [ $choice -eq 0 ]; then
+                        hcount=$(($hcount+1));
+                else
+                        tcount=$(($tcount+1));
+		fi
+		temp=$(($hcount-$tcount))
+		if [ $(($tcount-$hcount)) -eq 2 ]; then
+		break;
+		fi
+	done
+	}
+
+
 	end=1;
-	hcount=0;
-	tcount=0;
+	hcount=5;
+	tcount=5;
 
 	while [ $hcount -lt 21 ]
 	do
@@ -16,6 +35,10 @@
 			break;
 		elif [ $hcount -eq 21 ]; then
 			echo "Head wins by " $(($hcount-$tcount));
+
+		elif [ $tcount -eq 21 -a $hcount -eq 21 ]; then
+			tie
+			break;
 		fi
 	done
 
