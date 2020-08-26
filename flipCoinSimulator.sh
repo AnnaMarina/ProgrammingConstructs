d5e26b201ce4deafd363b7083520d7d8bf75edbe
AnnaMarina <anna.marina996@gmail.com>
2020-08-26 13:51:06 +0530

    Multiple flip

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+	end=1;
+	hcount=0;
+	tcount=0;
 
-	choice=$((RANDOM%2))
-	if [ $choice -eq 0 ]; then
-		echo "Heads. Win!";
-	else
-		echo "Tails. Loose! ";
-	fi
+	while [ $end -ne 0 ]
+	do
+		choice=$((RANDOM%2))
+		if [ $choice -eq 0 ]; then
+			hcount=$(($hcount+1));
+		else
+			tcount=$(($tcount+1));
+		fi
+	read -p "For exit enter 0   " end ;
+	done
+
+	echo "Head count " $hcount;
+	echo "Tail count " $tcount;
